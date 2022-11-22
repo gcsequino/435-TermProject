a37f8655fca3cb09679e01b4cd506add3b17ad85
Zach Fuchs <zachef221@gmail.com>
2022-11-22 15:47:17 -0700

    Get top N tags based on the number of posts with each tag

--- so_analysis.py
+++ so_analysis.py
@@ -1,13 +1,14 @@
 from pyspark import SparkContext
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import udf
-from pyspark.sql.types import MapType, StringType
-from sentiment_analysis import CommentSentimentAnalyer
+from pyspark.sql.functions import udf, explode, count
+from pyspark.sql.types import MapType, StringType, ArrayType
+from CommentSentimentAnalyer import CommentSentimentAnalyer
 import argparse
 import os
+import re
 
 
-def get_answered_posts(data_dir, limit=None):
+def get_answered_posts(spark, data_dir, limit=None):
     df = spark.read.parquet(os.path.join(data_dir, 'Posts.parquet'))
     # Rename _AcceptedAnswerId to TmpId so that there won't be a duplicate column in following join
     accepted_answer_id_df = df.select('_AcceptedAnswerId').distinct().withColumnRenamed('_AcceptedAnswerId', 'TmpId')
@@ -20,7 +21,16 @@ def get_answered_posts(data_dir, limit=None):
 
     return accepted_answer_posts_df
 
-
+def get_top_tags(spark: SparkSession, data_dir, limit=None):
+    posts = spark.read.parquet(os.path.join(data_dir, 'Posts.parquet'))
+    post_tags = posts.select("_Tags").dropna().withColumnRenamed("_Tags", "tags")
+    split_tag = udf(lambda tag: re.sub("(^<|>$)", "", tag).split("><"), ArrayType(StringType()))
+    split_tags = post_tags.select(split_tag(post_tags.tags).alias("tags"))
+    all_tags = split_tags.select(explode("tags").alias("tags"))
+    tags = all_tags.groupBy("tags").agg(count("tags").alias("counts")).orderBy("counts", ascending=False)
+    if limit:
+        tags = tags.limit(limit)
+    return tags
 
 
 if __name__ == "__main__":
@@ -51,7 +61,7 @@ if __name__ == "__main__":
     print(f"Got {accepted_answer_posts_df.count()} accepted answer posts")
 
     print("Running Sentiment Analysis")
-    context.addPyFile("sentiment_analysis.py")
+    context.addPyFile("CommentSentimentAnalyer.py")
     out_type = MapType(StringType(), StringType(), False)
     analyze_udf = udf(lambda b: CommentSentimentAnalyer().get_sentiment_polarity(b), out_type)
     accepted_answer_posts_df = accepted_answer_posts_df.withColumn("sentiments", analyze_udf("_Body"))
